55a54067111e4493fc523e60ed6b6df9334bfb8a
LvChengbin <lvchengbin59@gmail.com>
2022-12-26 16:31:38 +0800

    chore(literal): init repo
    init linting, testing, commiting scripts into repo

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/******************************************************************
+ * Copyright (C) 2022 LvChengbin
+ *
+ * File: literal/jest.config.js
+ * Author: LvChengbin<lvchengbin59@gmail.com>
+ * Time: 12/26/2022
+ * Description:
+ ******************************************************************/
+
+module.exports = {
+    projects : [
+        '<rootDir>/packages/literal'
+    ]
+};
